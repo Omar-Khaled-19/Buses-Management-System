724337f447f33f60ef244f9fd7c5634c9ec19300
rehab <rehab@LAPTOP-TJAT7SDD>
2023-12-02 22:57:54 +0200

    ...

--- Station.h
+++ Station.h
@@ -8,12 +8,12 @@ class Station
 {
 private:
 	int StationNum;
-	LinkedQueue<Bus> ForwardBusList;
-	LinkedQueue<Bus> BackwardBusList;
-	LinkedQueue<Passenger> WP_ForwardWaiting;
-	LinkedQueue<Passenger> WP_BackwardWaiting;
-	PriorityQueue<Passenger> NP_SP_ForwardWaiting;
-	PriorityQueue<Passenger> NP_SP_BackwardWaiting;
+	LinkedQueue<Bus*> ForwardBusList;
+	LinkedQueue<Bus*> BackwardBusList;
+	LinkedQueue<Passenger*> WP_ForwardWaiting;
+	LinkedQueue<Passenger*> WP_BackwardWaiting;
+	PriorityQueue<Passenger*> NP_SP_ForwardWaiting;
+	PriorityQueue<Passenger*> NP_SP_BackwardWaiting;
 public:
 	Station();
 	Station(int stationNum);
