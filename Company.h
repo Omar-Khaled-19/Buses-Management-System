642e6aa21f7249ed7acbac5977d353084ae310b4
rehab <rehab@LAPTOP-TJAT7SDD>
2023-12-02 12:20:18 +0200

    .

--- Company.h
+++ Company.h
@@ -1,5 +1,31 @@
 #pragma once
+#include "LinkedQueue.h"
+#include "LinkedList.h"
+#include "Event.h"
+#include "Passenger.h"
+#include "Bus.h"
+#include "Station.h"
+#include "Time.h"
 class Company
 {
+private:
+	LinkedQueue<Event*> EventPtrList;
+	LinkedList<Station> SationList;
+	int TripCheckupsNum;
+	Time TimeBetStaions;
+	LinkedQueue<Bus> BusList;
+	LinkedQueue<Bus> ForwardMovingBusList;
+	LinkedQueue<Bus> BackwardMovingBusList;
+	LinkedQueue<Bus> MovingWCBusList;
+	LinkedQueue<Bus> CheckupBusList;
+	LinkedQueue<Passenger> FinishedPassengerList;
+
+public:
+
+
+
+
 };
+
+
 
